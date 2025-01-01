24a6dfdfb84c4dfe83380e789f1e0ba5fd63fe0d
ilnursoft <ilnursoft@gmail.com>
2025-01-01 20:33:33 +0300

    add  выделение списка станций в конфигурационный файл

--- radio_app.py
+++ radio_app.py
@@ -4,36 +4,17 @@ import os
 
 app = Flask(__name__)
 
-# Список радиостанций
-radio_stations = [
-    {"name": "Китап радиосы", "url": "http://radio.tatmedia.com:8800/kitapfm"},
-    {"name": "Саф радиосы", "url": "https://c7.radioboss.fm:18335/stream"},
-    {"name": "Күнел Радиосы", "url": "http://live.kunelradio.ru:8000/128.mp3"},
-    {"name": "Курай ФМ", "url": "http://av.bimradio.ru:8066/kurai_mp3"},
-    {"name": "Юлдаш (башкирское радио)", "url": "https://radio.mediacdn.ru/uldash.mp3"},
-    {"name": "Радио Матур", "url": "http://radio.matur-tv.ru/radio/radio-mp3-128k"},
-    {"name": "Ватан сердасы (крым татар)", "url": "http://91.214.128.125:64000/vatan"},
-    {"name": "Ашҡаҙар радиоһы (Башҡортостан)", "url": "http://radio.mediacdn.ru/ashkadar.mp3"},
-    {"name": "Дулкын радиосы", "url": "http://radio.tatmedia.com:8800/Saba"},
-    {"name": "РадиоТМК (Казахстан)", "url": "http://a4.radioheart.ru:8036/RH13170"},
-    {"name": "Татарская поп музыка", "url": "http://pub0101.101.ru:8000/stream/pro/aac/64/246"},
-    {"name": "ТатРадиоЦентр", "url": "https://listen4.myradio24.com/trc"},
-    {"name": "Белем радиосы", "url": "https://radiobelem.ru/belem128"},
-    {"name": "Тәртип FM (93.1) Казань", "url": "https://radio.tatmedia.com:8443/tartipfm"},
-    {"name": "Татар радиосы", "url": "https://tatarradio.hostingradio.ru/tatarradio320.mp3"},
-    {"name": "Болгар радиосы", "url": "https://live.bolgarradio.com/b_aac_hifi.m3u8"},
-    {"name": "Татарстан Авазы", "url": "https://listen6.myradio24.com/gtrk"},
-    {"name": "Роксана радиосы", "url": "https://listen1.myradio24.com/2761"},
-    {"name": "Татарская Народная Музыка", "url": "https://pub0302.101.ru:8000/stream/pro/aac/64/262"},
-    {"name": "Әтнә театры интернет-радиосы", "url": "https://c2.radioboss.fm:18571/stream"},
-    {"name": "ТАТАРСТАН МӘДӘНИЯТЕ", "url": "https://c20.radioboss.fm:8560/stream"},
-    {"name": "Ногайское радио", "url": "http://radio05.ru:8000/nogayskoe_radio_128"},
-    {"name": "Таван радио (Чувашское)", "url": "http://icecast.ntrk21.ru:8000/tavan"},
-    {"name": "Жулдыз FM", "url": "http://91.201.214.229:8000/zhulduz"}
-]
-
+# Путь к файлу конфигурации радиостанций
+RADIO_STATIONS_FILE = "cfg/radio_stations.json"
 # Файл для сохранения последней станции
-LAST_STATION_FILE = "last_station.json"
+LAST_STATION_FILE = "cfg/last_station.json"
+
+def load_radio_stations():
+    """Загружаем радиостанции из файла конфигурации"""
+    with open(RADIO_STATIONS_FILE, "r") as file:
+        return json.load(file)
+
+
 
 def save_last_station(station):
     """Сохраняем последнюю станцию в файл"""
@@ -52,6 +33,7 @@ def get_last_station():
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+    radio_stations = load_radio_stations()
     last_station = get_last_station()
 
     if request.method == "POST":
