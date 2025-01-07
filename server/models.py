94e90cdd0efc5aaaf82a7a5ac93e1f09b00fbc1e
ilnursoft <ilnursoft@gmail.com>
2025-01-07 20:35:48 +0300

    fix model db

--- server/models.py
+++ server/models.py
@@ -12,8 +12,10 @@ class User(UserMixin, db.Model):
     password_hash = db.Column(db.String(128))
     auth_type = db.Column(db.String(20), nullable=False)  # local, google, etc.
 
-    # Связь один-ко-многим с избранными станциями
+    # Связи с другими таблицами
     favorites = db.relationship('Favorite', backref='user', lazy=True)
+    stations = db.relationship('UserStation', backref='user', lazy=True)
+    last_station_id = db.Column(db.Integer, db.ForeignKey('user_station.id'), nullable=True)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -22,9 +24,21 @@ class User(UserMixin, db.Model):
         return check_password_hash(self.password_hash, password)
 
 
+class UserStation(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    url = db.Column(db.String(200), nullable=False)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+
+    # Добавляем составной уникальный индекс
+    __table_args__ = (
+        db.UniqueConstraint('user_id', 'name', name='unique_station_name_per_user'),
+    )
+
+
 class Favorite(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    station_name = db.Column(db.String(100), nullable=False)
-    station_url = db.Column(db.String(200), nullable=False)
+    station_id = db.Column(db.Integer, db.ForeignKey('user_station.id'), nullable=False)
     position = db.Column(db.Integer, nullable=False)  # 1, 2, или 3
